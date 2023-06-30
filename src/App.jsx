eb2bdf3ef9cd1d9adbb67367f9d8283e33af0350
Justin <justinmathiue0@gmail.com>
2023-06-29 21:45:33 -0700

    work on todo app still one aLab behind

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 import Todo from './Components/Todo';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
@@ -9,15 +9,9 @@ export default class App extends React.Component {
   render() {
     return (
       <>
-   <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Todo />} />
-            <Route path="/settings" element={<SettingsForm />} />
-          </Routes>
-          <Footer />
-        </BrowserRouter>        <Header />
+        <Header />
         <Todo />
+        <SettingsForm/>
         <Footer />
       </>
     );
